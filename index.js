22188012e110decd4b08e90d9a3011a11aefdd24
Paulo do Vale <110866574+pebdv@users.noreply.github.com>
2022-08-16 23:26:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,19 @@
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
-// 👇️ named import
 import {BigButton, SmallButton} from './another-file';
-
-
 
  function App() {
   return (
     <div>
       <BigButton />
-
-      <hr />
-
+      <hr>
       <SmallButton />
     </div>
   );
 }
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-/*
-If you run this example in your create-react-app,
-you will receive a warning that there is no "key" provided for the list items.
-*/
-
 
 export default App;
